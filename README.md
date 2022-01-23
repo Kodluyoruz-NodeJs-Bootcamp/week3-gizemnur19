1d2e23451668b8c18cf4750b2108aeeb967fbff7
gizemnur19 <gnkafadar@gmail.com>
2022-01-23 15:54:25 +0300

    initial commit -  jwt auth examples with ts

---
+++ README.md
@@ -0,0 +1,17 @@
+## Gusto & RemoteTeam Bootcamp Week4 - gizem nur kafadar
+
+# Typescript JWTAuthentication MongoDB
+## Description
+This is an example nodejs application of how to Authentication of users with Typescript, Express, Mongoose, Bcrypt, JWT, etc.
+
+## Apps
+Using Postman or RestEasy to feed the api.
+
+## Usage
+
+$ npm install
+
+$ npm run dev (development)
+
+## License
+[MIT License](http://opensource.org/licenses/MIT).
---
+++ src/routes/auth.routes.ts
@@ -0,0 +1,15 @@
+import { Router } from "express";
+import {
+  checkRolesExists,
+  checkUsernameAndEmailExists,
+} from "../middlewares/checkSignUp";
+import { isAuth } from "../middlewares/verifyToken";
+import { profile, signIn, signUp } from "../controllers/auth/auth.controller";
+
+const router: Router = Router();
+
+router.post("/signin", isAuth, signIn);
+router.post("/signup", [checkRolesExists, checkUsernameAndEmailExists], signUp);
+router.get("/profile", isAuth, profile);
+
+export default router;
